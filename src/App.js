0387f705ef53b02c4cf7147ae259e880ab71f07c
zemiak <zemiak@gmail.com>
2018-11-13 21:24:23 +0100

    Getting images async

--- src/App.js
+++ src/App.js
@@ -2,60 +2,68 @@ import React from 'react';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 
-const photos = [
-    { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
-    { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
-    { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
-    { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
-    { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
-    { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
-    { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
-    { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
-    { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+const _PHOTOS = [
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2024.JPG', width: 4, height: 3 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2039.JPG', width: 1, height: 1 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2075.JPG', width: 3, height: 4 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2090.JPG', width: 3, height: 4 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2115.JPG', width: 3, height: 4 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_2125.JPG', width: 4, height: 3 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FP1040215.JPG', width: 3, height: 4 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FP1040228.JPG', width: 4, height: 3 },
+    { src: 'http://lenovo-server.local:8081/nasphotos/rest/files?path=2013%2F1301+Deti%2FIMG_0672.JPG', width: 4, height: 3 }
 ];
 
 
 class App extends React.Component {
     constructor() {
         super();
-        this.state = { currentImage: 0 };
         this.closeLightbox = this.closeLightbox.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
+        this.openImageOrFolder = this.openImageOrFolder.bind(this);
         this.gotoNext = this.gotoNext.bind(this);
         this.gotoPrevious = this.gotoPrevious.bind(this);
+
+        this.state = {
+            photos: [],
+            currentImage: 0
+        }
     }
 
-    openLightbox(event, obj) {
-      this.setState({
-          currentImage: obj.index,
-          lightboxIsOpen: true,
-      });
+    openImageOrFolder(event, obj) {
+        // decide:
+        // - if this is a folder - open it
+        // - if this is an image - open Lightbox
+
+        this.setState({
+            currentImage: obj.index,
+            lightboxIsOpen: true,
+        });
     }
 
     closeLightbox() {
-      this.setState({
-          currentImage: 0,
-          lightboxIsOpen: false,
-      });
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
     }
 
     gotoPrevious() {
-      this.setState({
-          currentImage: this.state.currentImage - 1,
-      });
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
     }
 
     gotoNext() {
-      this.setState({
-          currentImage: this.state.currentImage + 1,
-      });
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
     }
 
     render() {
         return (
             <div>
-                <Gallery photos={photos} onClick={this.openLightbox} />
-                <Lightbox images={photos}
+                <Gallery photos={this.state.photos} onClick={this.openImageOrFolder} />
+                <Lightbox images={this.state.photos}
                     onClose={this.closeLightbox}
                     onClickPrev={this.gotoPrevious}
                     onClickNext={this.gotoNext}
@@ -63,7 +71,13 @@ class App extends React.Component {
                     isOpen={this.state.lightboxIsOpen}
                 />
             </div>
-      )
+        )
+    }
+
+    componentDidMount() {
+        this.setState({
+            photos: _PHOTOS,
+        });
     }
 }
 
